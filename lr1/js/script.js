9aabd307eb53707f96534ddc1049ebddd3153036
Skipperini <73227227+Skipperini@users.noreply.github.com>
2021-09-09 20:53:35 +0300

    Add files via upload

---
+++ lr1/js/script.js
@@ -0,0 +1,18 @@
+var N = 24;
+alert(N + " Шкідіна Катерина Сергіївна");
+var a= 4*N-13, b = 2*N-6;
+function Func(){
+    a = document.form.a.value;
+    b = document.form.b.value;
+    var a1 = parseInt(a,a1);
+    var b2 = parseInt(b,b2);
+    var c = a+b;
+    document.write("<br>a+b= "+c);
+    c=a-b;
+    document.write("<br> a-b = "+c);
+    document.write("<br>a*b= "+(a*b));
+    document.write("<br>a/b= "+(a/b));
+    document.write("<br>a++ = "+a++);
+    document.write("<br>b-- = "+b--);
+    document.write("<br>a%b = " +(a%b));
+}
