af2fdc582c191b1a71d74df4bac4b7fd55b7e4a0
Skipperini <73227227+Skipperini@users.noreply.github.com>
2021-10-31 15:42:32 +0300

    Add files via upload

---
+++ Magic Ball/js/scr7.js
@@ -0,0 +1,25 @@
+var divtxt = document.createElement('div');
+var cont = document.createElement('div');
+var inputq = document.createElement('input');
+var divball = document.createElement('div');
+var divansver = document.createElement('span');
+inputq.style.marginTop='2%';
+divtxt.innerHTML = '<span>Input or imagine your question<span>';
+cont.className="cont";
+divansver.innerHTML='Click to ask';
+inputq.setAttribute('type','text');
+document.body.append(cont);
+cont.append(divtxt);
+cont.append(inputq);
+divball.classList.add('ball');
+var answers = ['Maybe.', 'Never!', 'NO.', 'The future is bleak.','Possibly.', 'Yes!','Spirits say yes',];
+divball.onclick = function () {
+    divball.classList.add('clshake');;
+    var answer = answers[Math.floor(Math.random() * answers.length)];
+    divansver.className="blinkcl";
+    divansver.innerHTML = answer;
+    divball.addEventListener( "animationend",  function() {
+    divball.classList.remove("clshake");
+    divansver.classList.remove("blinkcl")});}
+cont.append(divball);
+divball.append(divansver);
